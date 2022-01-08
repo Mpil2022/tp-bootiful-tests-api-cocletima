9b1dd3c4e8147217d2d2007567346f403cb6e0fc
Thibault Choné <43148335+thibch@users.noreply.github.com>
2022-01-08 09:13:59 +0100

    first commit

---
+++ src/main/java/fr/ul/tpbootifultestsapicocletima/TpBootifulTestsApiCocletimaApplication.java
@@ -0,0 +1,13 @@
+package fr.ul.tpbootifultestsapicocletima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBootifulTestsApiCocletimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpBootifulTestsApiCocletimaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ul/tpbootifultestsapicocletima/TpBootifulTestsApiCocletimaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ul.tpbootifultestsapicocletima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpBootifulTestsApiCocletimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
