71f4cb81966222e37e2e053b1378fa1378b95c43
Tabary Mathieu <43238044+TabaryM@users.noreply.github.com>
2022-01-08 09:33:06 +0100

    Create basic AdderService and its documentation

--- src/main/java/fr/ul/tpbootifultestsapicocletima/services/AdderService.java
+++ src/main/java/fr/ul/tpbootifultestsapicocletima/services/AdderService.java
@@ -9,18 +9,36 @@ import org.springframework.stereotype.Service;
 public class AdderService {
     private int num;
 
+    /**
+     * Getter for the number in the API memory
+     * @return this.num
+     */
     public int getNum() {
         return num;
     }
 
+    /**
+     * Setter for the number in the API memory
+     * @param num the new value
+     */
     public void setNum(int num) {
         this.num = num;
     }
 
+    /**
+     * Calculate the result of the sum of the number in the API memory and the number passed as parameter
+     * @param other the other number of the sum
+     * @return this.num + other
+     */
     public int add(int other) {
         return this.num + other;
     }
 
+    /**
+     * Update the value of the number in the API memory by the sum of it and the number passed as parameter
+     * @param other the other number of the sum
+     * @return the sum of the number in memory and the parameter
+     */
     public int accumulate(int other) {
         return this.num += other;
     }
